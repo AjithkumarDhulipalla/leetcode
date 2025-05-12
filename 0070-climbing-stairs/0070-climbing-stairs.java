a7ab8d4b15431007b382a6bf14793b1432796dc2
AjithkumarDhulipalla <154047243+AjithkumarDhulipalla@users.noreply.github.com>
2025-05-12 14:10:55 +0530

    Time: 0 ms (100%), Space: 40.7 MB (18.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    int[] memo=new int[46];
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        if (memo[n]!=0) return memo[n];
+        memo[n]=climbStairs(n-1)+climbStairs(n-2);
+        return memo[n];
+ }
+}
+
+// class Solution {
+//     public int climbStairs(int n) {
+//         if (n <= 2) 
+//             return n;
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// }
