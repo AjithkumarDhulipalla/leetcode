60445d7b9b72c06c6ed077816405afbf94c31f9c
AjithkumarDhulipalla <154047243+AjithkumarDhulipalla@users.noreply.github.com>
2025-06-09 13:23:49 +0530

    Time: 3 ms (50.99%), Space: 41.5 MB (23.92%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,14 +1,31 @@
-class Solution {
-    public int findTheWinner(int n, int k) {
-       Queue<Integer> q = new LinkedList<>();
-        for(int i=1;i<=n;i++)
-         q.add(i);
+// class Solution {
+//     public int findTheWinner(int n, int k) {
+//        Queue<Integer> q = new LinkedList<>();
+//         for(int i=1;i<=n;i++)
+//          q.add(i);
 
-        while(q.size()>1){
-            for(int i=1;i<k;i++)
-            q.add(q.poll());
-            q.poll();
+//         while(q.size()>1){
+//             for(int i=1;i<k;i++)
+//             q.add(q.poll());
+//             q.poll();
+//         }
+//         return q.poll();
+//     }
+// }
+
+class Solution {
+        public int findTheWinner(int n, int k) {
+            ArrayList<Integer> circle = new ArrayList<>();
+        for(int i = 1;i<=n;i++){
+            circle.add(i);
         }
-        return q.poll();
+        int cur_ind = 0;
+
+        while(circle.size() > 1){
+            int next_to_remove = (cur_ind + k - 1) % circle.size();
+            circle.remove(next_to_remove);
+            cur_ind = next_to_remove;
+        }
+        return circle.get(0);
     }
 }
